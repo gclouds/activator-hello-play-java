54dc41501565001076efb5ba40b48bed2698374b
gclouds <gaurav.tgqa@gmail.com>
2015-11-22 03:33:16 +0530

    Update Application.java

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -6,7 +6,7 @@ import play.mvc.Result;
 public class Application extends Controller {
     
     public static Result index() {
-        return ok(views.html.index.render("Hello Play Framework"));
+        return ok(views.html.index.render("Something new is coming soon, for queries: gaurav@drashtee.com or +91 8904667815"));
     }
     
 }
